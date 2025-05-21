0689241c79c07886a51a817600df791b5f403334
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2025-05-21 16:53:59 +0300

    Update app.js

--- home-manager-api/app.js
+++ home-manager-api/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'home-manager-project', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'browser', 'index.html'));
 });
 
 module.exports = app;
