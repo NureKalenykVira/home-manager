10fbdca847f0ba5df7d3a613a62a4c666c5e41d7
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2025-05-29 12:21:43 +0300

    Update environment.prod.ts

--- home-manager-project/src/environments/environment.prod.ts
+++ home-manager-project/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://home-manager-api-gma2axdzgabddzb7.northeurope-01.azurewebsites.net'
+  apiUrl: 'https://home-manager-api-gma2axdzgabddzb7.northeurope-01.azurewebsites.net/api'
 };
